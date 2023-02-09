db9be4f842966d2c13a2df5aaa8e322f3790bef4
Angie-540 <janiceangellah@gmail.com>
2023-02-09 15:12:29 +0300

    Dap - Add login styling

--- src/authentication/Login.tsx
+++ src/authentication/Login.tsx
@@ -1,15 +1,17 @@
 
 const Login = () => {
     return (
-        <div>
-            <h1>QA assistant</h1>
-            <div>
+        <div className="w-[40%] max-auto mx-auto p-4">
+            <h1 className="text-2xl text-center">QA ASSISTANT</h1>
+            <div className="p-4 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className = "block text-gray-700 text-sm font-bold mb-2 p-4" for="Username"> Username </div>
                 <form>
-                    <div>
-                    <input type="text" placeholder="Enter username" required />
+                    <div className="p-2">
+                    <input className="shadow appearance-none border-rounded w-full p-4 leading-tight" type="text" placeholder="Enter username" required />
                     </div>
-                    <div>
-                    <input type="password" placeholder="Enter password" required />
+                    <div className="p-2">
+                    <div className = "block text-gray-800 text-sm font-bold mb-2 p-4" for="Password"> Password </div>
+                    <input className="shadow appearance-none border-rounded w-full p-4 leading-tight w-full p-4" type="password" placeholder="Enter password" required />
                     </div>
                     <div>
                         <button type="submit">Login</button>
