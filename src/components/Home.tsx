ed076c8555790754378d5710195cbbdff0956cd9
Sainingo <mnduta57@gmail.com>
2023-02-09 13:38:39 +0300

    Dap - add styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <header className='bg-gray-100 flex justify-between p-4'>
+        <header className='bg-gray-100 flex justify-between p-6'>
             <a className='ml-8'>QA assistant</a>
             <nav className='mr-12'>
                 <ul className='flex gap-20'>
@@ -17,7 +17,7 @@ const Home = () => {
 
         <div className='w-[80%] mx-auto'>
             <div className='w-[80%] mx-auto m-4'>
-                <input className='w-full p-4 text-center text-1xl border border-slate-200 rounded-sm' type="text" placeholder='Search Patient'/>
+                <input className='w-full p-4 text-center text-xl border border-slate-200 rounded-sm' type="text" placeholder='Search Patient'/>
             </div>
         </div>
     </div>
