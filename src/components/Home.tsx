a3784c0faa65f8c416f1a4dee09ca126524bce93
Sainingo <mnduta57@gmail.com>
2023-02-10 08:34:00 +0300

    Dap - Fetch patient data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+const api_url = 'http://localhost:8089/openmrs/ws/rest/v1/patient?q=Sarah&v=default&limit=1'
 
 const Home = () => {
+    const [searchParams, setSearchParams] = useState()
+
+    const handleChange = (event: any) => {
+        const value = event.target.value;
+        setSearchParams(value)
+    }
+
+    let username = 'admin'
+    let password = 'Admin123'
+
+    useEffect(() => {
+        const searchData = async () => {
+            const result = await fetch(api_url, {
+                mode: 'no-cors',
+                method: "GET",
+                headers: {'Authorization': 'Basic ' + btoa(username +":" +password)},
+                redirect: 'follow'
+            })
+            const data = await result.json()
+            console.log(data)
+            return data
+       }
+        searchData()
+    }, [])
+
   return (
     <div>
         <header className='bg-gray-100 flex justify-between p-6'>
@@ -16,8 +42,16 @@ const Home = () => {
         </header>
 
         <div className='w-[80%] mx-auto'>
-            <div className='w-[80%] mx-auto m-4'>
-                <input className='w-full p-4 text-center text-xl border border-slate-200 rounded-sm' type="text" placeholder='Search Patient'/>
+            <div className='w-[80%] mx-auto m-4 flex gap-4'>
+                <input value={searchParams} onChange={handleChange} className='w-full p-4 text-center text-xl border border-slate-200 rounded-sm' type="text" placeholder='Search Patient'/>
+                <div className='m-2'>
+                <select className='p-4'>
+                    <option >Name</option>
+                    <option >UUID</option>
+                    <option>Age</option>
+                    <option>CC number</option>
+                </select>
+                </div>
             </div>
         </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server:{
+  //   proxy:{
+  //     '/openmrs': {
+  //       target: 'http://localhost:8089/',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
   test: {
     environment: 'jsdom',
   },
