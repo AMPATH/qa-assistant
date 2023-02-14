fd457d56cf81a1d89e94aea696d45118298edfe7
Sainingo <mnduta57@gmail.com>
2023-02-14 14:34:55 +0300

    DAP-2 Merge Search Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import storage from '../app/localStorage'
+import logo from '../../public/logo/ampath_Logo.png'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    const data = storage.loadData()
+    const currentUser = data?.user?.display
+
+    const handleLogout = () => {
+        localStorage.removeItem('userInformation');
+        localStorage.removeItem('authenticated')
+        window.location.reload();
+    }
+
+  return (
+    <div>
+         <header className='bg-themeColor flex justify-between p-4 items-center'>
+            <span className='cursor-pointer ml-3'>
+                <Link to="/"><img src={logo} width={150} /></Link>
+            </span>
+            <nav className='mr-12'>
+                <ul className='flex gap-20'>
+                    <li className='text-xl hover:bg-slate-300 p-4 rounded-sm cursor-pointer' onClick={handleLogout}>Log out</li>
+                </ul>
+            </nav>
+        </header>
+        <div className='relative p-2'>
+        <span className='absolute right-12 italic'>Logged in as <strong>{currentUser}</strong></span>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Header from './Header'
 import storage from '../app/localStorage'
-import Cookies from "js-cookie"
+// const api_url = 'openmrs/ws/rest/v1/patient?q=Sarah&v=default&limit=1'
+
 interface Result {
     [key: string]: any
 }
 const Home = () => {
     const [searchParams, setSearchParams] = useState<string>()
     const [results, setResults] = useState<Result[]>()
-    const navigate = useNavigate()
+
+
     const handleChange = (event: any) => {
         const value = event.target.value;
         setSearchParams(value)
     }
+
     const userInformation = localStorage.getItem("authenticated")
     if(userInformation!=="true"){
         window.location.href = "/login"
@@ -54,7 +58,7 @@ const logoutSession = () =>{
 
         <div className='w-[80%] mx-auto'>
             <div className='w-[80%] mx-auto m-4 flex gap-4'>
-                <input value={searchParams} onChange={handleChange} className='w-full p-4 text-center text-xl border border-slate-200 rounded-sm' type="text" placeholder='Search Patient'/>
+                <input value={searchParams} onChange={handleChange} className='bg-searchColor font-semi-bold outline-none w-full p-2 text-center text-xl border rounded-sm' type="text" placeholder='Search Patient'/>
                 <div className='m-2'>
                 <select className='p-4'>
                     <option >Name</option>
@@ -63,6 +67,29 @@ const logoutSession = () =>{
                     <option>CC number</option>
                 </select>
                 </div>
+            </div>
+        </div>
+        <div className='w-[80%] mx-auto mt-32'>
+            <div></div>
+            <div className='flex mx-auto w-[90%] justify-between rounded-md bg-slate-200 p-4'>
+                <div>Name</div>
+                <div>Age</div>
+                <div>Gender</div>
+                <div>Date of birth</div>
+            </div>
+            <div className='flex mx-auto w-[90%] justify-between p-4 cursor-pointer mt-1'>
+                {results ? (
+                    <>
+                   <Link to={`${results?.results[0]?.person.uuid}`}>
+                   <div className='hover:text-blue-600'>{results?.results[0]?.display.split(' ').slice(2).join(' ')}</div>
+                   </Link>
+                    <div>{results?.results[0]?.person.age}</div>
+                    <div>{results?.results[0]?.person?.gender}</div>
+                    <div>{results?.results[0]?.person.birthdate && new Date(results?.results[0]?.person.birthdate).toLocaleDateString()}</div>
+                   </> 
+                ) : (
+                    <p>Search a patient!!</p>
+                )}
             </div>
         </div>
     </div>
