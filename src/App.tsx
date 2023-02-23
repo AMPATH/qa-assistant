8fff169e888f65aaa73fb35c597a6433958dca4c
SharleenAwinja <awinjasharleen@gamil.com>
2023-02-23 11:55:22 +0300

    Created orders component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Login from "./authentication/Login/Login";
-import { BrowserRouter as Router,  Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-const App =  () => {
+import Orders from "./components/Orders/Orders";
+const App = () => {
   return (
     <Router>
-    <Routes>
-        <Route path="/login" element={<Login />}/> 
-        <Route path="/" element={<Home />}/>     
-    </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { Server } from 'https';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { Server } from "https";
 
 export default defineConfig({
-  server:{
-    proxy:{
-      '/openmrs': {
-        // target: 'https://dev3.openmrs.org',
-        target:'http://10.50.200.136:8089',
+  server: {
+    proxy: {
+      "/openmrs": {
+        target: "https://dev3.openmrs.org",
+        //target: "http://localhost:8088",
         // target:"https://kibana.ampath.or.ke/",
+        //target: "http://10.50.200.136:8089",
         changeOrigin: true,
         secure: false,
       },
@@ -16,8 +17,8 @@ export default defineConfig({
   },
   plugins: [react()],
   test: {
-    globals:true,
-    environment: 'jsdom',
+    globals: true,
+    environment: "jsdom",
   },
-  setupFiles: './tests/setup.ts',
+  setupFiles: "./tests/setup.ts",
 });
