79cd8eff2bd9dd7fc15c0ea08b660e7949780243
Angie-540 <janiceangellah@gmail.com>
2023-04-12 16:45:44 +0300

    modified:   vite.config.ts 	modified:   yarn.lock

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   server: {
-    // proxy: {
-    //   "/openmrs": {
-    //     target: "https://dev3.openmrs.org",
-    //     changeOrigin: true,
-    //     secure: false,
-    //   },
-    // },
+    proxy: {
+      "/openmrs": {
+        target: "https://dev3.openmrs.org",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
   test: {
